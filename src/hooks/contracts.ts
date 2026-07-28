47220eecf0cae48c326a0e7f03bfc75396bb4ea7
Morb <morbidejs@gmail.com>
2026-07-29 01:06:24 +0300

    Update contracts.ts

--- src/hooks/contracts.ts
+++ src/hooks/contracts.ts
@@ -2,7 +2,7 @@ import { getConfig } from "@/config"
 import type { EnvConfig } from "@/config"
 import { RelayerRewardsPool__factory } from "@vechain/vebetterdao-contracts/factories/RelayerRewardsPool__factory"
 import { XAllocationVoting__factory } from "@vechain/vebetterdao-contracts/factories/XAllocationVoting__factory"
-import { NavigatorRegistry__factory } from "@vechain/vebetterdao-contracts/factories/NavigatorRegistry__factory"
+import { NavigatorRegistry__factory } from "@vechain/vebetterdao-contracts/factories/navigator/NavigatorRegistry__factory"
 
 const env = (process.env.NEXT_PUBLIC_APP_ENV ?? "mainnet") as EnvConfig
 const config = getConfig(env)
